05a7ded35fa4c87408e510212ca086936bb4ae05
thesecondpioneer <thesecondpioneer@gmail.com>
2023-10-06 17:25:28 +0300

    new testing

--- main.cpp
+++ main.cpp
@@ -3,10 +3,28 @@
 
 using namespace dual;
 
+float f(float x) {
+    return std::pow(std::cos(x), std::sin(x));
+}
+
+float y(float x) {
+    return -pow(std::cos(x), std::sin(x) - 1) * std::pow(std::sin(x), 2) + std::pow(std::cos(x), std::sin(x) + 1) * std::log(std::cos(x));
+}
+
+void opt(float x, float &f, float &y) {
+    float sx = std::sin(x), cx = std::cos(x), ctsx = std::pow(cx, sx);
+    f = ctsx;
+    y = -(ctsx / cx) * sx * sx + ctsx * cx * std::log(cx);
+}
+
+Dual<float> autod(Dual<float> x){
+    return pow(cos(x), sin(x));
+}
+
 int main() {
-    dual::Dual<float> a(3.0, 1.0), b(7.0, 1.0);
-    std::cout << (1.0f + a).x << std::endl;
-    std::cout << (a + b).x << ' ' << (a + b).y << std::endl;
-    std::cout << log(a).x << ' ' << log(a).y << std::endl;
+    float func, deriv;
+    Dual<float> der = autod(Dual<float>(1.5f, 1.0f));
+    opt(1.5f, func, deriv);
+    std::cout << f(1.5) << ' ' << y(1.5) << std::endl << func << ' ' << deriv << std::endl << der.x << ' ' << der.y << std::endl;
     return 0;
 }
